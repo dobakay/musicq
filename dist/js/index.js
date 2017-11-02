be8271f1f19d3a2c0c4eb18ed141bddea641add1
DobaKay <dkralev90@gmail.com>
2017-11-02 07:31:15 -0700

    Add source maps generation

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,3 @@
 "use strict";
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbXSwic291cmNlUm9vdCI6IiJ9
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
+var path = require("path");
 var gulp = require("gulp");
-var ts = require("gulp-typescript");
 var merge = require('merge2');
 var clean = require('gulp-clean');
 var run = require('gulp-run');
+var ts = require("gulp-typescript");
+var sourcemaps = require('gulp-sourcemaps');
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('default', ['watch'], function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
-});
+gulp.task('default', ['watch']);
 
 gulp.task('clean-scripts', function () {
   return gulp.src('dist/*', {read: false})
@@ -18,15 +16,24 @@ gulp.task('clean-scripts', function () {
 
 
 gulp.task('scripts', ['clean-scripts'], function() {
-    var tsResult = gulp.src('src/*')
-                    .pipe(ts(tsProject));
+    var tsResult = tsProject
+        .src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
 
     return merge([  // Merge the two output streams, so this task is finished
-                    // when the IO of both operations are done. 
+                    // when the IO of both operations are done.
         tsResult.dts.pipe(gulp.dest('dist/definitions')),
-        tsResult.js.pipe(gulp.dest('dist/js'))
+        tsResult.js.pipe(sourcemaps.write({
+            // Return relative source map root directories per file.
+            sourceRoot: function (file) {
+                var sourceFile = path.join(file.cwd, file.sourceMap.file);
+                return path.relative(path.dirname(sourceFile), file.cwd);
+            }
+        })).pipe(gulp.dest('dist/js'))
     ]);
 });
+
 gulp.task('watch', ['scripts'], function() {
     gulp.watch('src/*', ['scripts']);
     return run('node dist/js/index.js').exec();
