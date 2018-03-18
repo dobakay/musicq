d71f6e6be4b539a1663c0ac944f240f0ced4c8f4
dobakay <dkralev90@gmail.com>
2018-03-18 11:48:30 +0200

    Adding AmplitudeJS typings

--- src/ts/routes/DownloadTubeRoute.ts
+++ src/ts/routes/DownloadTubeRoute.ts
@@ -58,28 +58,6 @@ export class DownloadTubeRoute extends BaseRoute {
         let videoID = req.params.videoID; //"ZTY8vlKO9hg";
         let videoURL = "http://www.youtube.com/watch?v=" + videoID;
         ytStream(videoURL).pipe(res);
-        // let youtubeDlUrl: string = "";
-        // let decoder = new StringDecoder("utf8");
-        // let options = { encoding: "utf8"};
-
-        // // # Spawn a child process to obtain the URL to the FLV(to the youtube vid)
-        // let youtubeDlUrlChild = exec("\.\\dist\\youtube_dl\\youtube-dl --simulate --get-url " + videoURL, (err, stdout, stderr) => {
-        //     console.log(stderr.toString());
-        //     //Converting the buffer to a string is a little costly so let's do it upfront
-        //     youtubeDlUrl = stdout.toString();
-        //     // there's a trailing '\n' returned from youtube-dl, let's cut it off
-        //     youtubeDlUrl = youtubeDlUrl.substring(0, youtubeDlUrl.length - 2);
-        //     res.contentType("audio/mpeg3");
-        //     let ffmpegChild = spawn("ffmpeg", ["-i", "pipe:0", "-acodec", "libmp3lame", "-f", "mp3", "-"]);
-        //     ffmpegChild.stdout.pipe(res);
-        //     try {
-        //         var webhook = "http://www.youtube.com/";
-        //         request({ url: youtubeDlUrl, uri: webhook, headers: { "Youtubedl-no-compression": "True" } }).pipe(ffmpegChild.stdin);
-        //     } catch (error) {
-        //         console.log(error);
-        //     }
-        // });
-
     }
 
     public downloadAudioToRoot(req: Request, res: Response, next: NextFunction) {
